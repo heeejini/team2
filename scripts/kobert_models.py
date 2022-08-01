61a6cd386fd2bedc947bde135070145ae4f61b80
KimGyunYeop <gyop0817@naver.com>
2022-08-01 14:39:18 +0900

    test입니다. 무시하세요

--- scripts/kobert_models.py
+++ scripts/kobert_models.py
@@ -122,8 +122,6 @@ class model_BoolQA(nn.Module):
         
         return output
     
-##############################END################
-
 ## TODO: WiC model (abandoned) ##
 class model_WiC_biSent_abandoned(nn.Module):
     def __init__(self):
@@ -134,81 +132,49 @@ class model_WiC_biSent_abandoned(nn.Module):
         
         #분류에 token embedding을 활용하는 경우
         #self.pooling = nn.AvgPool1d(3)
-        self.relu = nn.ReLU() #활성화 함수(0~1): nn.sigmoid, nn.relu(-를 0으로)
-        self.sigmoid = nn.Sigmoid()
+        
+        #self.relu = nn.ReLU() #활성화 함수(0~1): nn.sigmoid, nn.relu(-를 0으로)
+        #self.sigmoid = nn.Sigmoid()
         self.cosSim = nn.CosineSimilarity(dim=1, eps=1e-6)
         
-        #tokenEmbedding2개 붙인거를 linear 하게.       
-        self.liearConcat = nn.Linear(768*3,768) #tokenOutput concat할때,.
+        #output1,2 합쳐서 결과 도출 (768,1)
         self.linear = nn.Linear(768,1) #CLS: 768->2(binary)
         #self.pooling = nn.Linear(768,1)
-        self.softmax = nn.Softmax(dim=1) #dim=0(세로합=1),dim=1(가로합=1) #nn.LogSoftmax()
+        #self.softmax = nn.Softmax(dim=1) #dim=0(세로합=1),dim=1(가로합=1) #nn.LogSoftmax()
 
-    def forward(self, input_ids, token_type_ids, attention_mask, tokIdx_start, tokIdx_end): #MyModel에 input(input_ids,token_type_ids,attention_mask) 텐서들이 들어왔을때 forward를 실행.
-        iIds = input_ids.long()#.unsqueeze(0).long()
-        tok_typeIds = token_type_ids.long()#.unsqueeze(0).long()
-        attMask = attention_mask.long()#.unsqueeze(0)
-        
-        output=self.model_PLM(input_ids=iIds, token_type_ids=tok_typeIds, attention_mask=attMask) 
+        #word_index는 찾으려는 단어의 위치 
+    def forward(self, input_ids1, token_type_ids1, attention_mask1, word_index1,input_ids2, token_type_ids2, attention_mask2, word_index2): #MyModel에 input(input_ids,token_type_ids,attention_mask) 텐서들이 들어왔을때 forward를 실행.
+        iIds1 = input_ids1.long()#.unsqueeze(0).long()
+        tok_typeIds1 = token_type_ids1.long()#.unsqueeze(0).long()
+        attMask1 = attention_mask1.long()#.unsqueeze(0)
+
+        iIds2 = input_ids2.long()#.unsqueeze(0).long()
+        tok_typeIds2 = token_type_ids2.long()#.unsqueeze(0).long()
+        attMask2 = attention_mask2.long()#.unsqueeze(0)
+ 
+        #output1과 output2의 결과를 따로 계산
+        output1=self.model_PLM(input_ids1=iIds1, token_type_ids1=tok_typeIds1, attention_mask1=attMask1)
+        output2=self.model_PLM(input_ids2=iIds2, token_type_ids1=tok_typeIds2, attention_mask2=attMask2)
+
+        #print('output1 :',output1,'output2: ', output2)
+
+        #기존 주석 
         #print('output(bert-lhs)[CLS]: ',output['last_hidden_state'][:, 0, :].shape) #CLS token: torch.Size([batch_size, 768])
         #print('output shape: ',output['last_hidden_state'].shape, tokIdx_start.shape, tokIdx_end.shape) 
         # #torch.Size([bs, 100, 768]), torch.Size([100])*2개
-        
-        '''
-        #tokenIdx 뽑아서 embeddings에 저장.(mean-pooling)
-        embeddings = []
-        for idx, (iTok_s,iTok_e) in enumerate(zip(tokIdx_start,tokIdx_end)):
-            embedding = []
-            #print('token start/end: ',int(iTok_s),int(iTok_e)) #4 4로 똑같다고 할때, 아래와 같음.
-            for iTok in range(iTok_s,iTok_e+1):
-                embedding.append(output['last_hidden_state'][idx, iTok, :] )
-            #print(len(embedding), embedding[0].shape, type(embedding[0])) #list, torch.Size([1, 768])
-            embedding = torch.stack([embedding[i] for i in range(len(embedding))])
-            #print(embedding.shape, type(embedding)) #torch.Size([1, 768]) <class 'torch.Tensor'>
-            embedding = torch.mean(embedding, dim=0) #mean-pooling 
-            #print(embedding.shape, type(embedding)) #torch.Size([768]) <class 'torch.Tensor'>
-            embeddings.append(embedding)
-        #print('embeddings: ',type(embeddings), len(embeddings), embeddings[0].shape) #<class 'list'> 50 torch.Size([768])
-        output = torch.stack(embeddings, dim=0) #list[torch[768]]*100 -> 
-        #print('embeddings_stacked: ',output.shape) #torch.Size([50, 768])
-        '''
-        #tokenIdx 뽑아서 embeddings에 저장.(concat, +-1)
-        embeddings = []
-        for idx, (iTok_s,iTok_e) in enumerate(zip(tokIdx_start,tokIdx_end)):
-            embedding = []
-            #print('token start/end: ',int(iTok_s),int(iTok_e)) #4 4로 똑같다고 할때, 아래와 같음.
-            for iTok in range(iTok_s-1,iTok_s+2):
-                embedding.append(output['last_hidden_state'][idx, iTok, :] )
-            #print(len(embedding), embedding[0].shape, type(embedding[0])) #list, torch.Size([1, 768])
-            
-            embedding = torch.cat(embedding, dim=0) #Token전략: +-1씩 concat후, linearPooling
-            #print('cat:',embedding.shape, type(embedding))
-            embedding = self.liearConcat(embedding) # 3*torch.Size([768]) -> torch.Size([2304])
-            #print('lincat:',embedding.shape, type(embedding))
-            embedding = torch.stack([embedding[i] for i in range(len(embedding))])
-            #print('stack:',embedding.shape, type(embedding)) #torch.Size([1, 768]) <class 'torch.Tensor'>
-            
-            #embedding = torch.mean(embedding, dim=0) #Token전략: mean-pooling 
-            #print('pooling:',embedding.shape, type(embedding)) #torch.Size([768]) <class 'torch.Tensor'>
-            embeddings.append(embedding)
-        #print('embeddings: ',type(embeddings), len(embeddings), embeddings[0].shape) #<class 'list'> 50 torch.Size([768])
-        output = torch.stack(embeddings, dim=0) #list[torch[768]]*50 -> [50, 768]
-        #print('embeddings_stacked: ',output.shape) #torch.Size([50, 768])
 
+        output1=output1['last_hidden_state'][:,word_index1,:] #해당 단어만 사용 
+        output2=output2['last_hidden_state'][:,word_index2,:]
 
-        
-        #print('concatLinear: ',output.shape, output[0].shape, output[0][:20]) #
-        #self.sigmoid(output)
-        self.relu(output)
-        #print('activation Func: ',output.shape, output[0].shape, output[0][:20]) #torch.Size([50,768]) tensor(0.0005...
+        cos = nn.CosineSimilarity(dim=1, eps=1e-6)
+        #output1과 output2 코사인유사도 비교 
+        output = cos(output1, output2)
 
-        #output = self.linear(output)
-        #print('embeddings_reluLinear: ',output.shape) #torch.Size([50, 1])
-
-        
-        #output = self.softmax(output)
+        #output=self.linear(768,1)
+        #print('output : ',output)
         #print(output.shape)
-        
+
+        #코사인 유사도 비교후 -> 바로 그 값 리턴 
         return output
     
     
